3926562353cf5918e7b70cb58547f8d27657b022
gofei7818-hue <gofei7818@gmail.com>
2026-05-18 04:15:08 +0800

    Phase 0: add car entity scaffold

---
+++ src/vehicle/Car.ts
@@ -0,0 +1,28 @@
+import * as THREE from 'three';
+import { createCarModel } from './CarModel';
+
+export class Car {
+  readonly mesh: THREE.Group;
+  readonly position = new THREE.Vector3(0, 0, 0);
+  speed = 0;
+  heading = 0;
+
+  constructor() {
+    this.mesh = createCarModel();
+    this.mesh.position.copy(this.position);
+  }
+
+  update(deltaSeconds: number): void {
+    this.position.z -= this.speed * deltaSeconds;
+    this.mesh.position.copy(this.position);
+    this.mesh.rotation.y = this.heading;
+  }
+
+  reset(): void {
+    this.position.set(0, 0, 12);
+    this.speed = 0;
+    this.heading = 0;
+    this.mesh.position.copy(this.position);
+    this.mesh.rotation.set(0, 0, 0);
+  }
+}
